b8de72b15bc306db2f3d03dce99fc6407ddceb3f
M3D0S4 <82780619+M3D0S4@users.noreply.github.com>
2021-10-03 14:52:27 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,36 @@
+from discord.ext import commands
+from discord.ext.commands import has_permissions
+import time
+
+client = commands.Bot(command_prefix='.')
+
+message_list = []
+
+@client.command(pass_context=True)
+@has_permissions(administrator=True)
+async def duyuru(ctx):
+
+    message = (str(ctx.message.content[str(ctx.message.content).find(" "):]).strip())
+    if len(message)<3:
+        await ctx.send("Mesaj çok kısa")
+    message_list.append(message)
+
+
+@client.command()
+async def start(ctx):
+    while True:
+        for i in message_list:
+            time.sleep(21600)
+            await ctx.send(i)
+
+
+
+
+@duyuru.error
+async def duyuru_error(ctx, error):
+    await ctx.send("Bu rolu kullanamazsınız...")
+
+
+
+
+client.run("ODgwNDEzNzUzMzc0NTQ4MDY3.YSd7NA.fnObRz1X3K-EFXWqivxnwuxtSmA")
